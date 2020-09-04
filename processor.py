90e8c27e908c8f0bc5db6df337df930079918d70
Mark Hedrick <MarkoH17@users.noreply.github.com>
2020-09-04 16:08:05 -0600

    Improved LSB Encode Performance
    CHANGED - Numpy is now used to read the bits of the payload
    ADDED - Additional image details are provided with the getInfo method

--- processor.py
+++ processor.py
@@ -43,11 +43,13 @@ class LSBProcessor(Processor):
             self.src_img_pixels = cv2.cvtColor(self.src_img, cv2.COLOR_BGR2RGB)
 
     def getInfo(self):
+        print("\n\nHere are some stats about the image")
         print("-----------------------")
         print(f"SRC Path: {self.src_img_path}")
         print(f"Has Alpha: {self.has_alpha}")
         print(f"Total Pixels: {self.total_pixels}")
-        print("-----------------------")
+        print(f"Max Payload Size: {self.max_payload_size}")
+        print("-----------------------\n\n")
 
     def _lsb_embed(self, payload_bits):
         enc_pixels = np.copy(self.src_img_pixels)
@@ -60,23 +62,21 @@ class LSBProcessor(Processor):
                         return enc_pixels
 
                     pixel_val = self.src_img_pixels[i][j][k]
-                    payload_bit_val = int(payload_bits[payload_bit_index], 2)
-                    new_pixel_val = pixel_val & 0xFE | payload_bit_val
+                    new_pixel_val = pixel_val & 0xFE | payload_bits[payload_bit_index]
                 
                     enc_pixels[i][j][k] = new_pixel_val
                     payload_bit_index += 1
 
-                    #print("{0} {1} {2}".format(i, j, payload_bit_index))
         return enc_pixels
 
     def embedData(self, payload, dst_image):
-        print("Starting embed")
+        print("Embedding the payload in the cover image!")
         
         with open(payload, 'rb') as file: 
             payload_data = file.read()
-        payload_data += LSBProcessor.delimeter #stop marker
 
-        payload_bits = ''.join([format(i, "08b") for i in payload_data])
+        payload_data += LSBProcessor.delimeter #stop marker
+        payload_bits = np.unpackbits(np.frombuffer(payload_data, dtype="uint8", count=len(payload_data)))
         req_pixel_space = len(payload_bits)
 
         print("Embedding " + util.human_readable_size(req_pixel_space // 8, 2) + " of data in the cover image")
@@ -92,16 +92,23 @@ class LSBProcessor(Processor):
         else:
             enc_pixels = cv2.cvtColor(enc_pixels, cv2.COLOR_RGB2BGR)
 
-        cv2.imwrite(dst_image, enc_pixels)
+        cv2.imwrite(dst_image, enc_pixels, [cv2.IMWRITE_PNG_COMPRESSION, 9])
 
-        print("Finished embed")
+        print("Successfully embedded " +
+              util.human_readable_size(req_pixel_space // 8, 2) + " of data!")
 
 
 
     
     def extractData(self, payloadSavePath):
+        print("Extracting the payload from the image")
+
         dec_payload_bits = self._lsb_extract(self.src_img_pixels)
         payload_bytes = np.ndarray.tobytes(np.packbits(dec_payload_bits)).split(LSBProcessor.delimeter)[0]
+        
+        print("Successfully extracted " + util.human_readable_size(len(payload_bytes), 2) + " of data!")
+        print("Saving the payload to " + payloadSavePath)
+
         with open(payloadSavePath, 'wb') as file:
             file.write(payload_bytes)
           
